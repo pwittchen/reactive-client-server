872b845655ccebfb9e4c6e8641259df5b283a546
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-10-22 23:01:51 +0200

    adding slf4j logger

--- server/src/main/java/com/github/pwittchen/server/Main.java
+++ server/src/main/java/com/github/pwittchen/server/Main.java
@@ -1,28 +1,27 @@
 package com.github.pwittchen.server;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //TODO #1 apply RxJava2
 //TODO #2 create REST server
 //TODO #3 log sensor events
 public class Main {
-  private static Logger logger = LoggerFactory.getLogger(Main.class);
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
   public static void main(String args[]) {
     Vertx.vertx()
         .createHttpServer()
         .requestHandler(req -> {
           req.response().end("Hello from Vert.x");
-          String message = "host: %s, method: %s, uri: %s";
-          logger.info(String.format(message, req.host(), req.method().name(), req.uri()));
+          logger.info("{} {} {}", req.host(), req.method().name(), req.uri());
         })
         .listen(8080, handler -> {
           if (handler.succeeded()) {
-            System.out.println("server is running at http://localhost:8080/");
+            logger.info("server is running at http://localhost:8080");
           } else {
-            System.err.println("Failed to listen on port 8080");
+            logger.info("Failed to listen on port 8080");
           }
         });
   }
