9977564e036197d555e3622324d437e8b142f15c
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-11-02 23:20:35 +0100

    removing not necessary comment

--- server/src/main/java/com/github/pwittchen/server/Main.java
+++ server/src/main/java/com/github/pwittchen/server/Main.java
@@ -7,12 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
   private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
-  /**
-   * Starts Vert.x server and waits for the requests an exemplary request:
-   *
-   * curl "localhost:8080/sensor?reading=123,456,789"
-   */
+  
   public static void main(final String args[]) {
     final HttpServer server = Vertx
         .vertx()
