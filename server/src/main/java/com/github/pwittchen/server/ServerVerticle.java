977f9e0e958ed620f81dfe1f20f5ad7086a7b4dc
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-11-23 18:53:37 +0100

    converting Main class into Vert.x verticle

--- server/src/main/java/com/github/pwittchen/server/ServerVerticle.java
+++ server/src/main/java/com/github/pwittchen/server/ServerVerticle.java
@@ -1,14 +1,15 @@
 package com.github.pwittchen.server;
 
+import io.vertx.reactivex.core.AbstractVerticle;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.http.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
-  private static final Logger logger = LoggerFactory.getLogger(Main.class);
-  
-  public static void main(final String args[]) {
+public class ServerVerticle extends AbstractVerticle {
+  private static final Logger logger = LoggerFactory.getLogger(ServerVerticle.class);
+
+  @Override public void start() throws Exception {
     final HttpServer server = Vertx
         .vertx()
         .createHttpServer();
@@ -25,5 +26,9 @@ public class Main {
     server
         .rxListen(8080)
         .subscribe(httpServer -> logger.info("server is running at port 8080..."));
+  }
+
+  @Override public void stop() throws Exception {
+    logger.info("verticle stopped");
   }
 }
